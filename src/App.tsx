07fcd9cea96fafa9f640ae1dfc309ea73117cf4f
Kareem Taiye <olayinkahassan117@gmail.com>
2026-09-02 22:24:16 +0100

    add: search page router

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import PaperDetails from "./pages/papers/PaperDetails";
 import { Tasks } from "./pages/tasks/Task";
 import { AuthProvider } from "./context/AuthContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+
 import Settings from "./pages/settings/Settings";
 import { Metrics } from "./pages/metrics/Metrics";
 import Search from "./pages/Search/Search";
@@ -31,7 +32,9 @@ export default function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="papers" element={<Papers />} />
             <Route path="papers/:id" element={<PaperDetails />} />
+            {/* fghj */}
             <Route path="search" element={<Search />} />
+            {/* fghj */}
             <Route path="tasks" element={<Tasks />} />
             <Route path="settings" element={<Settings />} />
             <Route path="metrics" element={<Metrics />} />
